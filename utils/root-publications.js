5edb4030e7fa8f8a9d50866307498512caaab209
Nishant Totla <nishanttotla@gmail.com>
2015-05-10 13:33:45 -0700

    Function for creating adjacency list

--- utils/root-publications.js
+++ utils/root-publications.js
@@ -1,6 +1,15 @@
+// create adjacency list for the graph, as a list of lists
 function createAdjacencyList() {
   var links = forceGlobal.links();
-  for(var i=0; i<links.length; i++) {
-
+  var numNodes = forceGlobal.nodes().length;
+  var adjList = [];
+  // initialize the adjacency list
+  for(var i=0; i<numNodes; i++) {
+    adjList.push([]);
   }
+
+  for(var i=0; i<links.length; i++) {
+    adjList[links[i].target.id].push(links[i].source.id);
+  }
+  return adjList;
 }
