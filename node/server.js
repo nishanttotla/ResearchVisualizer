6ca4761c8e70136d0491db6484e7aadab54dfd66
Nishant Totla <nishanttotla@gmail.com>
2015-05-30 23:18:09 -0700

    1. Updated server to perform db queries (buggy) 2. Updated todo 3. Installed async node module

--- node/server.js
+++ node/server.js
@@ -1,7 +1,23 @@
 var http = require('http');
 var url = require('url');
+var async = require('async');
+
+// modules for database connection
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert')
+var ObjectId = require('mongodb').ObjectID;
+var dbUrl = 'mongodb://localhost:27017/test';
 
 const PORT = 8001;
+
+var retObjArray = [];
+
+var findPublications = function(db, id, callback) {
+   var cursor = db.collection('publications').find({"id":id});
+   console.log("query done!")
+   var ret = cursor.toArray();
+   callback(ret);
+};
 
 var server = http.createServer(function(request, response){
   // ignore the request for the favicon
@@ -11,11 +27,27 @@ var server = http.createServer(function(request, response){
     var queryObject = url.parse(request.url, true).query;
     console.log(queryObject);
     console.log(request.url);
+    var idRequested = parseInt(queryObject.id);
+
+    MongoClient.connect(dbUrl, function(err, db) {
+      assert.equal(null, err);
+      console.log("Opened database connection!")
+      findPublications(db, idRequested, function(ret) {
+          console.log("Closing database connection!");
+          db.close();
+          retObjArray = ret;
+      });
+    });
 
     // create response
-    retObj = {"id":0,"title":"Scala","authors":["Jack","Mike"],"abstract":"","keywords":[],"type":0,"venue":"POPL","year":2013}
+    retObj = retObjArray[0];
+
+    retObjArray = [];
+
     response.writeHead(200, {'Content-Type': 'application/json'});
-    response.write(JSON.stringify(retObj));
+    if(retObj != null) {
+      response.write(JSON.stringify(retObj));
+    }
     response.end();
   }
 });
